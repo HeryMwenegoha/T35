8c217e427b2a6e9abde83011db31b6c2ed65c4fa
HeryMwenegoha <hery_amani@yahoo.com>
2022-03-06 16:35:26 +0000

    Added some simple serial output
    This commit adds simple serial output to the main file.

--- T35/src/main.cpp
+++ T35/src/main.cpp
@@ -13,18 +13,22 @@ void setup()
 {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(57600);
 }
 
 void loop()
 {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED is HIGH");
 
   // wait for a second
   delay(1000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED is LOW");
 
    // wait for a second
   delay(1000);
